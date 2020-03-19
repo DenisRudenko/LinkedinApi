4c68f34e37f3c6af5556fbf2afc3391842e4afd8
DenisRudenko <DenisRudenko1986@gmail.com>
2020-03-19 21:23:28 +0200

    removed actual credentials

--- init.php
+++ init.php
@@ -1,8 +1,8 @@
 <?php
-define('CLIENT_ID', '86evxh2cayuw75');
-define('CLIENT_SECRET', 'xmKwN7FuDM8XpFVU');
+define('CLIENT_ID', '86evxh2cayuw75r');
+define('CLIENT_SECRET', 'xmKwN7FuDwM8XpFVU');
 define('REDIRECT_URI', 'https://seo-beraten.de/callback.php');
 define('SCOPES', 'r_emailaddress,r_liteprofile,w_member_social');
-define('ACCESS_TOKEN', 'AQUpUHexbGy1k6S-oPjNPuOCLmEcYrNCcKxl20OcSygIEq3ayyvZMgsNY_ILkfbMcm78k6TFFMq9Rx-JFKZZ-J7Qxitn-LjNrElztI_UItDGkTQAi0AM9BPKhl6GgJvRFb2yUaoaVPJJoPtlmapU10SWJPsoGAbCDxQmx2_9hEDQ8iCh8wRWJdHT62NP6rHA_V52QcZI901G6RqnR_8reVdex3Hr-LYRt-N_ofL49CSv8w9MMs4g3ggWRn3n9kqF8jkmjmoOXF8odfPQawyWq5h3t-nPx7BYpky1vTCw6OgpQlKPkod1lGsgDzSwIgjkj84yufFStpfAop85J9khkHkdwWAwug');
-define('LNKD_ID', 'ymBUv8r8KA');
+define('ACCESS_TOKEN', 'AQUpUHexqbGy1k6S-oPjNPuOCLmEcYrNCcKxl20OcSygIEq3ayyvZMgsNY_ILkfbMcm78k6TFFMq9Rx-JFKZZ-J7Qxitn-LjNrElztI_UItDGkTQAi0AM9BPKhl6GgJvRFb2yUaoaVPJJoPtlmapU10SWJPsoGAbCDxQmx2_9hEDQ8iCh8wRWJdHT62NP6rHA_V52QcZI901G6RqnR_8reVdex3Hr-LYRt-N_ofL49CSv8w9MMs4g3ggWRn3n9kqF8jkmjmoOXF8odfPQawyWq5h3t-nPx7BYpky1vTCw6OgpQlKPkod1lGsgDzSwIgjkj84yufFStpfAop85J9khkHkdwWAwug');
+define('LNKD_ID', 'ymBUrv8r8KA');
 ?>
